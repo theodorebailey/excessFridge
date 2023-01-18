e6230b4459d5454dc29f8069dce7995aad8db3a4
Theodore Bailey <tawbailey@hotmail.com>
2023-01-18 11:53:27 +0000

    "Removed empty file"

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import "bulma/css/bulma.min.css";
 // import statement for our app 
 import App from './App';
 
+// import browser router from react router dom to remove page referesh
 import { BrowserRouter } from 'react-router-dom'; 
 
 // Takes App component and renders into our HTML page
 
+// const root refers to HTML index
+// strict mode provides warning messages
+// Browser router allows use of Route elements 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
